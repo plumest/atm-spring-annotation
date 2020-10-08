ff4a4eb2a996cb3a5f65bdd8033cef16a6c0b3a6
plumest <chayathon.k@ku.th>
2020-10-08 10:20:15 +0700

    Implementation ATM with Spring annotaion-based

--- src/main/java/atm/DataSource.java
+++ src/main/java/atm/DataSource.java
@@ -1,7 +1,12 @@
 package atm;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 import java.util.Map;
 
+@Component
+@Primary
 public interface DataSource {
     Map<Integer,Customer> readCustomers();
 }
